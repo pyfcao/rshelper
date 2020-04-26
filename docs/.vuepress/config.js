fea44d6614530dfdcea9ae0ab529d338b8e5db84
caoyufu <pyfcao@163.com>
2020-04-26 22:52:41 +0800

    【2020/4/26】第一次提交

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,50 @@
+module.exports = {
+  title: '动迁系统 - 帮助文档',
+  description: '为动迁系统的使用提供帮助',
+  base: '/help/',// 设置站点根路径
+  head: [
+    ['link', {
+      rel: 'icon',
+      href: `/favicon.ico`
+    }]
+  ],
+  themeConfig : {
+    logo: '/logo.png',
+    nav : [
+      { text: '使用说明', link: '/guide' },
+      { text: '开发文档', link: '/develop' },
+      { text: '其他', link: '/other' }
+    ],
+    sidebar: {
+      '/' : [
+        {
+          title: '主页',   // 必要的
+          path: '/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+          collapsable: false, // 可选的, 默认值是 true,
+          sidebarDepth: 2,    // 可选的, 默认值是 1
+        },
+        {
+          title: '使用说明',   // 必要的
+          path: 'guide',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+          collapsable: false, // 可选的, 默认值是 true,
+          sidebarDepth: 2,    // 可选的, 默认值是 1
+        },
+        {
+          title: '开发文档',   // 必要的
+          path: 'develop',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+          collapsable: false, // 可选的, 默认值是 true,
+          sidebarDepth: 2,    // 可选的, 默认值是 1
+        },
+        {
+          title: '其他',   // 必要的
+          path: 'other',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+          collapsable: false, // 可选的, 默认值是 true,
+          sidebarDepth: 2,    // 可选的, 默认值是 1
+        }
+      ]
+    }
+  },
+  dest: './docs/.vuepress/dist',
+  ga: '',
+  evergreen: true,
+}
